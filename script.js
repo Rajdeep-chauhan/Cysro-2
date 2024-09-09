79b95c0c8dfe4e4f8d5987e2ad2af6abaa9c559e
Rajdeep-chauhan <141706314+Rajdeep-chauhan@users.noreply.github.com>
2024-09-09 15:32:47 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-console.log("Welcome to CodePen Clone");
-const sidebar = document.querySelector('.sidebar');
-const navButtons = document.querySelectorAll('.nav-buttons button');
-
-navButtons.forEach((button) => {
-  button.addEventListener('click', () => {
-    sidebar.classList.toggle('open');
-  });
-});
